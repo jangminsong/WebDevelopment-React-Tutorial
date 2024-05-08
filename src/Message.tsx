05d50e86f18e03518b6e360278af746823f46f12
Jangmin Song <jsong3@bates.edu>
2024-05-08 15:25:11 -0400

    React Tutorial

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+//Pascal Casing
+function Message(){
+    const name = "Jangmin";
+        if (name)
+            return <h1>Hello World {name}</h1>;
+        return <h1>Hello World</h1>;
+    }
+    
+    
+
+export default Message;
